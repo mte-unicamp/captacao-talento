370b4f2f02a4138188bc54b6d67347d8f1c04cef
Gustavo Soares <gusnsoares@gmail.com>
2019-10-04 19:07:31 -0300

    contact_reminder func + ClosedCompany model + minor tweaks

--- bot/models.py
+++ bot/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
-# from django.core.mail import send_mail
+from django.core.mail import send_mail
+import os
+import datetime as dt
 # from trello_helper import Helper
 # from django.core.validators import MinValueValidator, MaxValueValidator
-# import datetime as dt
 
 
 class Hunter(models.Model):
@@ -16,8 +17,8 @@ class Hunter(models.Model):
     of hunters)
     """
 
-    email = models.EmailField(max_length=100, primary_key=True)
-    name = models.CharField(max_length=100)
+    email = models.EmailField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
     list_id = models.CharField(max_length=100, null=True)
 
     class Meta:
@@ -38,6 +39,10 @@ class Seller(Hunter):
         return Company.objects.filter(seller=self).count()
 
     @property
+    def contact_list(self):
+        return list(Company.objects.filter(seller=self))
+
+    @property
     def closed_count(self):
         return Company.objects.filter(seller=self, status=Global.CLOS).count()
 
@@ -49,7 +54,7 @@ class Contractor(Hunter):
 
     @property
     def contact_count(self):
-        return Company.objects.filter(contractor=self).count()
+        return ClosedCompany.objects.filter(contractor=self).count()
 
 
 class Global(models.Model):
@@ -82,32 +87,46 @@ class Global(models.Model):
     REJE = 'reje'
     NEGO = 'nego'
     CLOS = 'clos'
-    # contractor stages
+    # fee type
     BRON = 'bron'
     SILV = 'silv'
     GOLD = 'gold'
     DIAM = 'diam'
+    # contract type
+    REGU = 'regu'
     STAR = 'star'
     OTHE = 'othe'
     CANC = 'canc'
+    # payment form
+    BOLE = 'bole'
+    TRAN = 'tran'
 
     STAGE_SELLER_LIST = [FIRS, NANS, INTE, REJE, NEGO, CLOS]
 
-    STAGE_CONTRACTOR_LIST = [BRON, SILV, GOLD, DIAM, STAR, OTHE, CANC]
+    FEE_TYPE_LIST = [BRON, SILV, GOLD, DIAM, OTHE]
+
+    CONTRACT_TYPE_LIST = [REGU, STAR, CANC]
+
+    PAYMENT_FORM_LIST = [BOLE, TRAN]
 
     NO_REMINDER = [NANS, REJE, CLOS]
 
     STAGE_SELLER_CHOICES = (
-        (FIRS, 'Initial Contact'), (NANS, 'No Answer'),
-        (INTE, 'Interested'), (REJE, 'Rejected'),
-        (NEGO, 'Negotiation'), (CLOS, 'Closed'),
+        (FIRS, 'Initial Contact'), (NANS, 'No Answer'), (INTE, 'Interested'),
+        (REJE, 'Rejected'), (NEGO, 'Negotiation'), (CLOS, 'Closed'),
     )
 
-    STAGE_CONTRACTOR_CHOICES = (
-        (BRON, 'Bronze'), (SILV, 'Silver'),
-        (GOLD, 'Gold'), (DIAM, 'Diamond'),
-        (STAR, 'Startup'), (OTHE, 'Other'),
-        (CANC, 'Cancelled'),
+    FEE_TYPE_CHOICES = (
+        (BRON, 'Bronze'), (SILV, 'Silver'), (GOLD, 'Gold'),
+        (DIAM, 'Diamond'), (OTHE, 'Other'),
+    )
+
+    CONTRACT_TYPE_CHOICES = (
+        (REGU, 'Regular'), (STAR, 'Startup'), (CANC, 'Cancelled'),
+    )
+
+    PAYMENT_FORM_CHOICES = (
+        (BOLE, 'Boleto'), (TRAN, 'Transfer'),
     )
 
     # deadline variables
@@ -118,19 +137,16 @@ class Global(models.Model):
 class Company(models.Model):
     """docstring for Company"""
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     card_id = models.CharField(max_length=100, primary_key=True)
     seller = models.ForeignKey('Seller', on_delete=models.SET_NULL, null=True)
-    contractor = models.ForeignKey(
-        'Contractor', on_delete=models.SET_NULL, null=True)
     category = models.CharField(max_length=4, choices=Global.CATEGORY_CHOICES)
     seller_stage = models.CharField(
         max_length=4, choices=Global.STAGE_SELLER_CHOICES, default=Global.FIRS)
-    contractor_stage = models.CharField(
-        max_length=4, choices=Global.STAGE_CONTRACTOR_CHOICES, blank=True)
-    last_activity = models.DateTimeField(default=timezone.now)
-    date_closed = models.DateTimeField()
+    last_activity = models.DateField(default=timezone.now)
+    date_closed = models.DateField(default=dt.date.today)
     comments_number = models.IntegerField(default=0)
+    main_contact = models.EmailField(blank=True)
 
     @property
     def inactive_time(self):
@@ -163,11 +179,54 @@ class Company(models.Model):
         return self.name
 
 
+class ClosedCompany(Company):
+
+    company = models.OneToOneField('Company', on_delete=models.CASCADE)
+    sec_card_id = models.CharField(max_length=100, primary_key=True)
+    contractor = models.ForeignKey(
+        'Contractor', on_delete=models.SET_NULL, null=True)
+    fee_type = models.CharField(
+        max_length=4, choices=Global.FEE_TYPE_CHOICES)
+    contract_type = models.CharField(
+        max_length=4, choices=Global.CONTRACT_TYPE_CHOICES)
+    payment_form = models.CharField(
+        max_length=4, choices=Global.PAYMENT_FORM_CHOICES)
+    intake = models.IntegerField()
+    needs_receipt = models.BooleanField()
+    stand_size = models.IntegerField()
+    stand_pos = models.CharField(max_length=10)
+    custom_stand = models.BooleanField()
+    payday = models.DateField()
+
+    class Meta:
+        verbose_name_plural = 'closedCompanies'
+
+
 class Reminder(models.Model):
     """docstring for Reminder"""
 
     # attributes
 
-    # methods
-
-    pass
+    @classmethod
+    def contact_reminder(seller):
+        companies = []
+        for c in seller.contact_list:
+            if c.needs_reminder:
+                companies.append(c)
+        subject = "[Talento 2020] Lembrete de Captação"
+        from_email = os.environ['EMAIL_HOST_USER']
+        line_list = ["Olá, {seller}!",
+                     "",
+                     "Você precisa entrar em contato com a(s)\
+                      empresa(s) a seguir:",
+                     ]
+        for c in companies:
+            line_list.append("\t* {company} ({email});")
+        line_list.append("Qualquer dúvida ou problema favor entrar \
+            em contato pelo endereço {reply}")
+        message = "\n".join(line_list).format(seller=seller.name,
+                                              company=c.name,
+                                              email=c.main_contact,
+                                              reply=from_email)
+        recipient_list = [seller.email]
+        send_mail(subject, message, from_email, recipient_list)
