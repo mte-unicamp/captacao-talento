91ad896b05df8acb4909793107174affa1cd2676
Gustavo Soares <gusnsoares@gmail.com>
2019-12-23 22:58:18 -0300

    custom host and port for db

--- captacao_talento/settings.py
+++ captacao_talento/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
         'NAME': get_env_var('DATABASE_NAME'),
         'USER': get_env_var('DATABASE_USER'),
         'PASSWORD': get_env_var('DATABASE_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': get_env_var('DATABASE_HOST'),
+        'PORT': get_env_var('DATABASE_PORT'),
     }
 }
 
