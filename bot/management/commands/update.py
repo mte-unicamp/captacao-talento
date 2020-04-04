8a917d6177b01a942956aff5186ecaf681b29415
Gustavo Soares <gusnsoares@gmail.com>
2020-04-04 16:15:14 -0300

    auto aupdate functions

--- bot/management/commands/update.py
+++ bot/management/commands/update.py
@@ -1,11 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
-
+from trello_helper.models import Updater
+import os
 
 class Command(BaseCommand):
     help = 'help'
 
     def handle(self, *args, **options):
         try:
-            pass
+            for c in Company.objects.all():
+                Updater.set_last_activity(c)
+                Updater.label_update(os.environ['SALES_BOARD_ID'])
         except Exception as e:
             raise CommandError('Update failed')
